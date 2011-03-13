3c8bcbe715dfc140de100dcdafb27c53d46dc9ee
Johan Nordberg <its@johan-nordberg.com>
2011-03-13 16:26:12 +0100

    Add missing changes to Notification class (thanks xcode4)

--- Notification.h
+++ Notification.h
@@ -7,11 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <WebKit/WebKit.h>
 
 @interface Notification : NSObject {
-@private
-    
+  NSString *_title;
+  NSString *_message;
+
+  WebScriptObject *ondisplay;
+  WebScriptObject *onerror;
+  WebScriptObject *onclose;
 }
+
+- (id)initWithTitle:(NSString *)title message:(NSString *)message;
+
+- (void)show;
+- (void)cancel;
+
+@property (nonatomic, retain) WebScriptObject *ondisplay;
+@property (nonatomic, retain) WebScriptObject *onerror;
+@property (nonatomic, retain) WebScriptObject *onclose;
 
 @end
--- Notification.m
+++ Notification.m
@@ -7,23 +7,74 @@
 //
 
 #import "Notification.h"
-
+#import <Growl/Growl.h>
 
 @implementation Notification
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
+@synthesize ondisplay, onerror, onclose;
+
+- (id)initWithTitle:(NSString *)title message:(NSString *)message {
+  if ((self = [self init])) {
+    _title = [title retain];
+    _message = [message retain];
+  }
+  return self;
 }
 
-- (void)dealloc
-{
-    [super dealloc];
+- (void)show {
+  // TODO: subclass Notification for growl integration instead.
+  //       also keep track of notifications and post onclose event appropriately
+
+  [GrowlApplicationBridge notifyWithTitle:_title
+                              description:_message
+                         notificationName:@"IRCCloudMessage"
+                                 iconData:nil
+                                 priority:0
+                                 isSticky:NO
+                             clickContext:nil
+                               identifier:[NSString stringWithFormat:@"%p", self]];
+
+  if (ondisplay && [ondisplay isMemberOfClass:[WebScriptObject class]]) {
+    [ondisplay callWebScriptMethod:@"call" withArguments:nil];
+  }
+}
+
+- (void)cancel {
+  // TODO
+}
+
++ (BOOL)isKeyExcludedFromWebScript:(const char *)name {
+  NSString *str = [[[NSString alloc] initWithCString:name encoding:NSASCIIStringEncoding] autorelease];
+  if ([str isEqualToString:@"ondisplay"] || [str isEqualToString:@"onclose"] ||
+      [str isEqualToString:@"onerror"]) {
+    return NO;
+  }
+  return YES;
+}
+
++ (NSString *)webScriptNameForSelector:(SEL)sel {
+  if (sel == @selector(show)) {
+    return @"show";
+  } else if (sel == @selector(cancel)) {
+    return @"cancel";
+  }
+  return nil;
+}
+
++ (BOOL)isSelectorExcludedFromWebScript:(SEL)sel {
+  if (sel == @selector(show) || sel == @selector(cancel)) {
+    return NO;
+  }
+  return YES;
+}
+
+- (void)dealloc {
+  [_title release];
+  [_message release];
+  [ondisplay release];
+  [onclose release];
+  [onerror release];
+  [super dealloc];
 }
 
 @end
